37d6ff428c5f3d534ed96ea8a1918e653f8cd590
Софья Еськина <eskinasofa@gmail.com>
2026-04-15 23:03:51 +0500

    first commit

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from urllib import response
+from django.shortcuts import render
+from django.http import  HttpResponse
+
+def index(request):
+    context={
+        'title':'home',
+        'content':'главная мтраница магазина под названием ""',
+    }
+    return render(request,'main/index.html',context)
+
+def about(request):
+    return render(request,'main/about.html')
