f80d458a50cdbedea2ea02bd91ecdbcbe336cb5c
Софья Еськина <eskinasofa@gmail.com>
2026-04-16 22:00:42 +0500

    main app is ready

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from main import views
+app_name='main'
+urlpatterns = [
+    path('',views.index,name = 'index'),
+    path('about/',views.about,name = 'about'),
+    path('contact/',views.contact,name = 'contact'),
+    path('delivery/',views.delivery,name = 'delivery'),
+]
+
--- main/views.py
+++ main/views.py
@@ -4,10 +4,30 @@ from django.http import  HttpResponse
 
 def index(request):
     context={
-        'title':'home',
-        'content':'главная cтраница магазина под названием ""',
+        'title':'Нome - главная',
+        'content':"Магазин мебели HOME",
     }
     return render(request,'main/index.html',context)
 
 def about(request):
-    return render(request,'main/about.html')
+    context={
+        'title':'Нome - О нас',
+        'content':"О нас",
+        'text_on_page':'текст какой крутой этот магазин'
+    }
+    return render(request,'main/about.html', context)
+def contact(request):
+    context={
+        'title':'Нome - Контакты',
+        'content':"Контактная информация",
+        'text_on_page':'8 800 555 35 35'
+    }
+    return render(request,'main/about.html', context)
+
+def delivery(request):
+    context={
+        'title':'Нome - Доставка и оплата',
+        'content':"Условия доставки и оплаты",
+        'text_on_page':'текст условия я не придумала'
+    }
+    return render(request,'main/about.html', context)
